79429157ecc25203cf8ba7b3337807de86f8959c
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-05-02 19:35:27 +0800

    Coba hapus login dll

--- MyKantin/DetailMakanan.Designer.cs
+++ MyKantin/DetailMakanan.Designer.cs
@@ -55,9 +55,10 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(139, 41);
+            this.label1.Location = new System.Drawing.Point(185, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 22);
+            this.label1.Size = new System.Drawing.Size(253, 27);
             this.label1.TabIndex = 1;
             this.label1.Text = "Detail Makanan";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -67,9 +68,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 408);
+            this.label2.Location = new System.Drawing.Point(16, 502);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 29);
+            this.label2.Size = new System.Drawing.Size(163, 36);
             this.label2.TabIndex = 4;
             this.label2.Text = "Rp. 10.000";
             // 
@@ -77,9 +79,10 @@
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(345, 408);
+            this.label3.Location = new System.Drawing.Point(460, 502);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(22, 21);
+            this.label3.Size = new System.Drawing.Size(29, 26);
             this.label3.TabIndex = 7;
             this.label3.Text = "-";
             // 
@@ -87,9 +90,10 @@
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(382, 408);
+            this.label4.Location = new System.Drawing.Point(509, 502);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(22, 21);
+            this.label4.Size = new System.Drawing.Size(29, 26);
             this.label4.TabIndex = 8;
             this.label4.Text = "1";
             // 
@@ -97,9 +101,10 @@
             // 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(426, 408);
+            this.label5.Location = new System.Drawing.Point(568, 502);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(22, 21);
+            this.label5.Size = new System.Drawing.Size(29, 26);
             this.label5.TabIndex = 9;
             this.label5.Text = "+";
             // 
@@ -107,18 +112,20 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 478);
+            this.label6.Location = new System.Drawing.Point(16, 588);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(145, 20);
+            this.label6.Size = new System.Drawing.Size(178, 25);
             this.label6.TabIndex = 10;
             this.label6.Text = "About Nasi Goreng";
             // 
             // label7
             // 
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(13, 509);
+            this.label7.Location = new System.Drawing.Point(17, 626);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(425, 78);
+            this.label7.Size = new System.Drawing.Size(567, 96);
             this.label7.TabIndex = 11;
             this.label7.Text = resources.GetString("label7.Text");
             // 
@@ -126,9 +133,10 @@
             // 
             this.pictureBox6.BackColor = System.Drawing.Color.Black;
             this.pictureBox6.Image = global::MyKantin.Properties.Resources.keranjang_belanja;
-            this.pictureBox6.Location = new System.Drawing.Point(99, 637);
+            this.pictureBox6.Location = new System.Drawing.Point(132, 784);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(40, 41);
+            this.pictureBox6.Size = new System.Drawing.Size(53, 50);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 13;
             this.pictureBox6.TabStop = false;
@@ -136,9 +144,10 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::MyKantin.Properties.Resources.rectengel_2;
-            this.pictureBox5.Location = new System.Drawing.Point(84, 590);
+            this.pictureBox5.Location = new System.Drawing.Point(112, 726);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(320, 134);
+            this.pictureBox5.Size = new System.Drawing.Size(427, 165);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 12;
             this.pictureBox5.TabStop = false;
@@ -146,9 +155,10 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::MyKantin.Properties.Resources.rectengel;
-            this.pictureBox4.Location = new System.Drawing.Point(330, 388);
+            this.pictureBox4.Location = new System.Drawing.Point(440, 478);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(131, 74);
+            this.pictureBox4.Size = new System.Drawing.Size(175, 91);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 6;
             this.pictureBox4.TabStop = false;
@@ -157,9 +167,10 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::MyKantin.Properties.Resources.makanan1;
-            this.pictureBox3.Location = new System.Drawing.Point(-2, 89);
+            this.pictureBox3.Location = new System.Drawing.Point(-3, 110);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(484, 293);
+            this.pictureBox3.Size = new System.Drawing.Size(645, 361);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
@@ -168,9 +179,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::MyKantin.Properties.Resources.love;
-            this.pictureBox2.Location = new System.Drawing.Point(400, 31);
+            this.pictureBox2.Location = new System.Drawing.Point(533, 38);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(39, 32);
+            this.pictureBox2.Size = new System.Drawing.Size(52, 39);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -178,12 +190,14 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MyKantin.Properties.Resources.panah;
-            this.pictureBox1.Location = new System.Drawing.Point(40, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(53, 38);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(38, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(51, 39);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label8
             // 
@@ -191,9 +205,10 @@
             this.label8.BackColor = System.Drawing.Color.Black;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(145, 649);
+            this.label8.Location = new System.Drawing.Point(193, 799);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(81, 20);
+            this.label8.Size = new System.Drawing.Size(102, 25);
             this.label8.TabIndex = 14;
             this.label8.Text = "Keranjang";
             // 
@@ -203,18 +218,19 @@
             this.label9.BackColor = System.Drawing.Color.Black;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(294, 649);
+            this.label9.Location = new System.Drawing.Point(392, 799);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(87, 20);
+            this.label9.Size = new System.Drawing.Size(106, 25);
             this.label9.TabIndex = 15;
             this.label9.Text = "Rp. 10.000";
             // 
             // DetailMakanan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(482, 713);
+            this.ClientSize = new System.Drawing.Size(643, 878);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.pictureBox6);
@@ -231,6 +247,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DetailMakanan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DetailMakanan";
--- MyKantin/DetailMakanan.cs
+++ MyKantin/DetailMakanan.cs
@@ -31,5 +31,10 @@ namespace MyKantin
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

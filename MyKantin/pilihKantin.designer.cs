289712592c55b5de9c9af0e0c8ca7346cf322f00
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-05-10 04:11:53 +0800

    03.11

--- MyKantin/pilihKantin.designer.cs
+++ MyKantin/pilihKantin.designer.cs
@@ -172,6 +172,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(482, 713);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.labelPilihPembayaran);
